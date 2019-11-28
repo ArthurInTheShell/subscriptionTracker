2036066dfa0a11c338b4cf1927ba03bbdd7a5ed8
ArthurInTheShell <junhaochenarthur@outlook.com>
2019-11-28 14:30:46 +0800

    Add monthly cost and markdown output.

---
+++ subscription.md
@@ -0,0 +1,7 @@
+| Subscription Name | Total Cost | Duration(Month) | Cost per Month |
+| ----------- | ----------- | ----------- | ----------- |
+| Leetcode | 159 | 12 | 13.25 |
+| Netflix | 12.99 | 1 | 12.99 |
+| iCloud | 2.99 | 1 | 2.99 |
+| Amazon Prime | 59 | 12 | 4.92 |
+**Monthly subscription cost is 34.15**
--- subscription.py
+++ subscription.py
@@ -3,9 +3,32 @@ import csv
 def data_read():
     with open('subscription.csv', 'r', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            print(', '.join(row))
+        sum = 0
+        with open('subscription.md','w',newline='') as mdout:
+            mdout.write('| Subscription Name | Total Cost | Duration(Month) | Cost per Month |\n')
+            mdout.write('| ----------- | ----------- | ----------- | ----------- |\n')
+            for row in spamreader:
+                print(', '.join(row))
+                cpm = float(row[1])/float(row[2])
+                mdout.write('| ' + ' | '.join(row) + ' | ' + '{:04.2f}'.format(cpm) +' |\n')
+                sum += cpm
+            print('Monthly subscription cost is '+ '{:04.2f}'.format(sum))
+            mdout.write('**Monthly subscription cost is '+ '{:04.2f}'.format(sum)+'**')
 
+def data_output():
+    with open('subscription.csv', 'r', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        sum = 0
+        with open('subscription.md','w',newline='') as mdout:
+            mdout.write('| Subscription Name | Total Cost | Duration(Month) | Cost per Month |\n')
+            mdout.write('| ----------- | ----------- | ----------- | ----------- |\n')
+            for row in spamreader:
+                print(', '.join(row))
+                cpm = float(row[1])/float(row[2])
+                mdout.write('| ' + ' | '.join(row) + ' | ' + '{:04.2f}'.format(cpm) +' |\n')
+                sum += cpm
+            print('Monthly subscription cost is '+ '{:04.2f}'.format(sum))
+            mdout.write('**Monthly subscription cost is '+ '{:04.2f}'.format(sum)+'**')
 
 def data_entered(sub_name, total, duration):
     with open('subscription.csv', 'a', newline='') as csvfile:
@@ -20,13 +43,18 @@ def prompt_for_input():
     data_entered(sub_name, total, duration)
 
 def main():
-    selection = int(input('Enter 1 to start entering subscription\n2 to read from data\n3 to exit:\n'))
-    while selection < 3:
+    selection = int(input('Enter\n1 - enter subscription\n'+
+    '2 - read, update subscription\n3 - read, update subscription and exit\n4 - exit:\n'))
+    while selection < 4:
         if selection == 1:
             prompt_for_input()
         elif selection == 2:
             data_read()
-        selection = int(input('Enter 1 to start entering subscription\n2 to read from data\n3 to exit:\n'))
+        elif selection == 3:
+            data_output()
+            break
+        selection = int(input('Enter\n1 - enter subscription\n'+
+        '2 - read, update subscription\n3 - read, update subscription and exit\n4 - exit:\n'))
     
     
 
